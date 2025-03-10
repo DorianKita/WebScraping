e4a41d075678df8919e7c95d593908bcd434666a
Dorian Kita <dorian.kita90@gmail.com>
2025-03-10 21:30:51 +0100

    created a simple program that scrapes technological news website and displays the highest ranked news.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from operator import index
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,10 +9,40 @@ response = requests.get(url=URL)
 webpage = response.text
 
 soup = BeautifulSoup(webpage, 'html.parser')
-first_article = soup.find(name='a', class_='storylink').getText()
-article_link = soup.find(name='a', class_='storylink').get('href')
-article_upvote = soup.find(name='span', id='score_40725924').getText()
-print(article_upvote)
+first_article = soup.find_all(name='a', class_='storylink')
+article_link = soup.find_all(name='a', class_='storylink')
+article_upvote = soup.find_all(name='span', class_='score')
+
+articles = [article.getText() for article in first_article]
+article_links = [link.get('href') for link in article_link]
+article_upvotes = [int(vote.getText().split()[0]) for vote in article_upvote]
+
+highest_score = max(article_upvotes)
+highest_score_index = article_upvotes.index(highest_score)
+
+highest_score_article = {
+    'article': articles[highest_score_index],
+    'url': article_links[highest_score_index],
+    'score': article_upvotes[highest_score_index]
+}
+
+for key, value in highest_score_article.items():
+    print(f'{key}: {value}')
+
+# a= 0
+# for vote in article_upvotes:
+#     if vote > a:
+#         a = vote
+#
+# if a in article_upvotes:
+#     index = article_upvotes.index(a)
+# print(articles[index])
+# print(article_links[index])
+
+# collection = zip(articles,article_links,article_upvotes)
+# for item in collection:
+#     print(item)
+
 
 
 
