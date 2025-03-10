8df1a8a1ccd6c00d8b7b0a94ee22a80c95e16d6d
Dorian Kita <dorian.kita90@gmail.com>
2025-03-10 20:22:26 +0100

    learning selecting specific tags

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ with open('website.html') as website:
     contents = website.read()
 
 soup = BeautifulSoup(contents, 'html.parser')
+a_tags = soup.find_all('a')
+
+h1 = soup.find(name='h1',id='name')
+print(h1)
+
+h3 = soup.find(name='h3', class_='heading')
+print(h3)
+
+for a in a_tags:
+    # print(a.getText())
+    print(a.attrs['href'])
